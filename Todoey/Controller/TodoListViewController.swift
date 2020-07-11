b1eed3b11e1f951af8ef5cdf5621381fdc4ac454
yossi102 <yossi0102@gmail.com>
2020-07-11 15:00:56 +0300

    End SearchBar

--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -57,8 +57,8 @@ class TodoListViewController: UITableViewController {
         
         itemArrey[indexPath.row].done = !itemArrey[indexPath.row].done
         
-//        context.delete(itemArrey[indexPath.row])
-//        itemArrey.remove(at: indexPath.row)
+        //        context.delete(itemArrey[indexPath.row])
+        //        itemArrey.remove(at: indexPath.row)
         saveItems()
         
         
@@ -116,15 +116,43 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems() {
+    func loadItems(with request : NSFetchRequest<Item> = Item.fetchRequest()) {
         
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
         do{
             itemArrey = try context.fetch(request)
         } catch {
             
             print(error)
         }
+        tableView.reloadData()
     }
 }
+
+//MARK - searchBar Delegate
+
+extension TodoListViewController : UISearchBarDelegate{
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@",  searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+}
+
+
+
+
 
